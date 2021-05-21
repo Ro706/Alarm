4ae930bf418fa6d0641dbeb64417761006bfe24d
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-05-21 11:25:21 +0530

    Update timmer.py

--- timmer.py
+++ timmer.py
@@ -6,7 +6,7 @@ from os import system
 title = str(input('title: '))
 for i in tq(range(100)):
     sp(0.1)
-a = str(input('enter alam:'))
+a = str(input('enter alarm:'))
 hour = str(dt.now().strftime('%H:%M'))
 while hour != a:
     sp(0.1)
